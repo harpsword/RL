6d6c33be9ad64cc590cec50a7ade372bf97f2f84
harpsword <harpswordyyl@gmail.com>
2019-05-09 17:43:04 +0800

    add more parts

--- DQN/dqn.py
+++ DQN/dqn.py
@@ -3,29 +3,104 @@ import os
 import json
 import click
 import gym
+import logging
+import torch
+import numpy as np
 
-from policy import Q_Net
+from policy import Q_Net, process
 from data import Data
 
+LogFoler = os.path.join(os.getcwd(), 'log')
+FRAME_SKIP = 4
+GAMMA = 0.9
+init_epsilon = 0.5
+decay_every_timestep = 100000
+epsilon_decay = 0.5
+final_epsilon = 0.1
+
+# training
+batchsize = 32
 
 # experience replay storage
 D = Data()
 
-def train(cfg):
-    interact()
-    
-    
+def train(cfg, model, env):
+    env.frameskip = 1
+    epsilon = init_epsilon 
+    optimizer = torch.optim.rmsprop(model.parameters())
+    for episode in range(1, cfg['game']['episode']):
+        obs = env.reset()
+        obs_list = [obs, obs, obs, obs]
+        state_now = process(sequence, cfg)
+        
+        break_is_true = False
+        for step in range(cfg['game']['timesteplimit']):
+            if np.randon.rand() <= epsilon:
+                action = np.random.randint(env.action_space.n)
+            else:
+                action = model(state_now).argmax().item()
+            obs_list = []
+            ep_r = 0
+            for i in range(FRAME_SKIP):
+                obs, reward, done, _ = env.step(action)
+                obs_list.append(obs)
+                ep_r += reward
+                if done:
+                    break_is_true = True
+            while len(obs_list) < FRAME_SKIP:
+                # when len(obs_list) < 4, done=True
+                # like start state obs_list, stack more end state together
+                obs_list.append(obs_list[-1])
+            sequence = [state_now, action, ep_r, process(obs_list), done]
+            D.push(sequence)
+
+            # sample data
+            # train model
+            if len(D.data) >= batchsize:
+                selected_data = np.random.sample(D.data, batchsize)
+                state_batch = [batch[0] for batch in selected_data]
+                target = []
+                for i in range(len(batchsize)):
+                    if selected_data[i][4]:
+                        target.append(selected_data[i][2])
+                    else:
+                        target.append(selected_data[i][2]+GAMMA*model(state_batch[i]).max().item())
+                # build dataset
+                # build dataloader
+                # update
+
+            if break_is_true:
+                break
+
+
+def setup_logging(logfile):
+    if logfile == 'default.log':
+        timenow = time.localtime(time.time())
+        logfile = str(timenow.tm_year)+'-'+str(timenow.tm_mon)+'-'+str(timenow.tm_mday)
+        indx = 1
+        while logfile+'-'+str(indx)+'.log' in os.listdir(LogFolder):
+            indx += 1
+        logpath = os.path.join(LogFolder, logfile+'-'+str(indx)+'.log')
+    else:
+        logpath = os.path.join(LogFolder, logfile)
+    logging.basicConfig(filename=logpath,
+                        level=logging.INFO,
+                        format='%(asctime)s - %(levelname)s - %(message)s',
+                        datefmt='%m/%d/%Y %I:%M:%S %p')
 
 
 @click.command()
-@click.option('--exp_file')
-@click.option('--log_dir')
-def main(exp_file, log_dir):
+@click.option('--expfile')
+@click.option('--logfile', default='default.log', help='the name of log file')
+def main(expfile, logfile):
     assert exp_file is not None
     if exp_file:
         with open(exp_file, 'r') as f:
             cfg = json.loads(f.read())
-    log_dir = os.path.expanduser(log_dir) if log_dir
+    setup_logging(logfile)
+    env = gym.make(cfg['game']['gamename'])
+    model = Q_Net(env.action_space.n)
+    train(cfg, model, env)
 
 
 
--- DQN/policy.py
+++ DQN/policy.py
@@ -1,7 +1,7 @@
 # model come from [mnih et al. 2013]
-
 import torch
 import torch.nn as nn
+import numpy as np
 import torch.nn.functional as F
 
 from torchvision import transforms
@@ -31,3 +31,15 @@ class Q_Net(nn.Module):
         x = F.relu(self.conv2(x))
         x = F.relu(self.fc1(x))
         return self.fc2(x)
+
+
+def process(x_list):
+    new_xlist = []
+    for x in x_list:
+        x = transforms.ToPILImage()(x).convert('RGB')
+        # output of trans: (1, 84, 84)
+        x = trans(x)
+        x = x.reshape(1, 1, 84, 84)
+        new_xlist.append(x)
+        x = torch.cat(new_xlist, 1)
+
