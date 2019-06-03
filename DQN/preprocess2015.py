f76fff5a3e90a281bc62dc1c5cedf3b8142a7ec5
yyl <harpswordyyl@gmail.com>
2019-06-03 22:45:24 +0800

    separate preprocess to one file

--- DQN/preprocess2015.py
+++ DQN/preprocess2015.py
@@ -1,10 +1,7 @@
-# model come from [mnih et al. 2013]
 import torch
-import torch.nn as nn
 import numpy as np
-import torch.nn.functional as F
-
 from torchvision import transforms
+from collections import deque
 
 trans = transforms.Compose(
     [
@@ -14,39 +11,25 @@ trans = transforms.Compose(
     ]
 )
 
-class Q_Net(nn.Module):
-    '''
-    input: (N, 4, 84, 84)
-    batch size = N = 32
-    '''
-    def __init__(self, ac_space):
-        super(Q_Net, self).__init__()
-        self.conv1 = nn.Conv2d(4, 16, kernel_size=8, stride=4)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=4, stride=2)
-        self.fc1 = nn.Linear(9*9*32, 256)
-        self.fc2 = nn.Linear(256, ac_space)
-
-    def forward(self, x:torch.tensor):
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = x.view(-1, 9*9*32)
-        x = F.relu(self.fc1(x))
-        return self.fc2(x)
-
-
-def process(x_list):
-    new_xlist = []
-    for x in x_list:
-        x = transforms.ToPILImage()(x).convert('RGB')
-        # output of trans: (1, 84, 84)
-        x = trans(x)
-        x = x.reshape(1, 1, 84, 84)
-        new_xlist.append(x)
-    x = torch.cat(new_xlist, 1)
-    return x
-
 class ProcessUnit(object):
+    """
+    Initialization:
+    PU = ProcessUnit(Frame_skip)
 
+    # when encounter one obs
+    obs = env.reset()
+    PU.step(obs)
+
+    ...... add other obs
+
+    for i in range(Frame_skip):
+        # get torch tensor
+        # PU will transform the 4 frames in self.frame_list into a tensor(1,4,84,84)
+        action = model(PU.to_torch_tensor())
+        obs, reward, done, _ = env.step(action)
+        # PU will delete the older frame in PU.frame_list, then add new obs
+        PU.step(obs)
+    """
     def __init__(self, length):
         self.length = length
         self.frame_list = deque(maxlen=length)
@@ -76,3 +59,5 @@ class ProcessUnit(object):
         x = trans(x)
         x = x.reshape(1, 1, 84, 84)
         return x
+
+
