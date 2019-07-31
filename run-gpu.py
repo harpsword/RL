8b6288f21a2c2136d7eb0cfacf8d021bd64f6468
yyl <harpswordyyl@gmail.com>
2019-07-31 22:05:05 +0800

    change the folder setting

--- run-gpu.py
+++
@@ -1,7 +0,0 @@
-
-import ray
-from RL.AC import a2cgpu
-
-ray.init()
-
-a2cgpu.main("Amidar-v0")
--- run.py
+++
@@ -1,8 +0,0 @@
-
-import ray
-from RL.AC import a2c
-from RL.PolicyGradient import ppo2
-
-ray.init()
-
-ppo2.main("Amidar-v0")
