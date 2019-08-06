2fd34774c5d1f6729c8c73e9665bc79a0a844bf4
yyl <harpswordyyl@gmail.com>
2019-08-06 21:22:12 +0800

    add some code for DDPG model design

--- code/util/model2.py
+++ code/util/model2.py
@@ -1,6 +1,6 @@
 """
 It's for MuJoCo Environment
-Stochasitic and Deterministic Policy
+Deterministic Policy (DDPG)
 TODO
 """
 
@@ -18,38 +18,7 @@ class AbstractPolicy(nn.Module):
     def __init__(self):
         super(AbstractPolicy, self).__init__()
 
-    def act(self, x):
-        x = self.forward(x)
-        x = F.softmax(x, dim=1)
-        return np.random.choice(self.ac_space, p=x.detach().numpy()[0])
-
-    def act_with_prob(self, x):
-        x = self.forward(x)
-        x = F.softmax(x, dim=1)
-        action = np.random.choice(self.ac_space, p=x.detach().numpy()[0])
-        prob = x[0][action]
-        return action, prob
-
-    def return_prob(self, x):
-        x = self.forward(x)
-        x = F.softmax(x, dim=1)
-        return x
-
-    def _initialize_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, math.sqrt(2. / n))
-                if m.bias is not None:
-                    m.bias.data.zero_()
-            elif isinstance(m, nn.Linear):
-                m.weight.data.normal_(0, 0.01)
-                m.bias.data.zero_()
-            elif isinstance(m, nn.BatchNorm2d):
-                nn.init.constant(m.weight, 1)
-                nn.init.constant(m.bias, 0)
-
-    def __initialize_weights_2(self):
+    def __initialize_weights(self):
         for m in self.modules():
             if isinstance(m, (nn.Conv2d, nn.Linear)):
                 nn.init.orthogonal(m.weight())
@@ -58,83 +27,51 @@ class AbstractPolicy(nn.Module):
                 nn.init.constant(m.bias, 0)
 
 
-class Policy2015(AbstractPolicy):
-    '''
-    input: (N, 4, 84, 84)
-    batch size = N 
-    '''
-    def __init__(self, ac_space):
-        super(Policy2015, self).__init__()
-        self.conv1 = nn.Conv2d(4, 32, kernel_size=8, stride=4)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
-        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
-        self.fc1 = nn.Linear(7*7*64, 512)
-        self.fc2 = nn.Linear(512, ac_space)
-        self.ac_space = ac_space
-        self._initialize_weights()
+class Policy(AbstractPolicy):
 
-    def forward(self, x:torch.tensor):
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
-        x = x.view(-1, 7*7*64)
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        return x
+    def __init__(self, state_dim, action_dim, action_lim):
+        super(Policy, self).__init__()
+        self.state_dim = state_dim
+        self.action_dim = action_dim
+        self.action_lim = action_lim
+        self.fc1 = nn.Linear(self.state_dim, 64)
+        self.fc2 = nn.Linear(64, 64)
+        self.fc3 = nn.Linear(64, self.action_dim)
+        self.__initialize_weights()
 
-
-class Policy2013(AbstractPolicy):
-    '''
-    input: (N, 4, 84, 84)
-    batch size = N = 32
-    '''
-    def __init__(self, ac_space):
-        super(Policy2013, self).__init__()
-        self.conv1 = nn.Conv2d(4, 16, kernel_size=8, stride=4)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=4, stride=2)
-        self.fc1 = nn.Linear(9*9*32, 256)
-        self.fc2 = nn.Linear(256, ac_space)
-        self.ac_space = ac_space
-        self._initialize_weights()
-
-
-    def forward(self, x:torch.tensor):
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = x.view(-1, 9*9*32)
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        return x
+    def forward(self, x):
+        x = F.tanh(self.fc1(x))
+        x = F.tanh(self.fc2(x))
+        x = F.tanh(self.fc3(x))
+        # TODO: the use of action_lim
+        return x * self.action_lim
 
 
 class Value(nn.Module):
     """
     Input: (N, 4, 84, 84)
     """
-    def __init__(self):
+    def __init__(self, state_dim, action_dim):
         super(Value, self).__init__()
-        self.conv1 = nn.Conv2d(4, 16, kernel_size=8, stride=4)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=4, stride=2)
-        self.fc1 = nn.Linear(9*9*32, 256)
-        self.fc2 = nn.Linear(256, 1)
+        self.state_dim = state_dim
+        self.action_dim = action_dim
+        self.fcs1 = nn.Linear(self.state_dim, 64)
+        self.fcs2 = nn.Linear(64, 64)
+        self.fca1 = nn.Linear(self.action_dim, 64)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, 1)
         self._initialize_weights()
 
-    def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = x.view(-1, 9*9*32)
-        x = F.relu(self.fc1(x))
-        return self.fc2(x)
+    def forward(self, x, a):
+        # TODO: the existence of fca1 for action input
+        pass   
 
-    def _initialize_weights(self):
+    def __initialize_weights(self):
         for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, math.sqrt(2. / n))
-                if m.bias is not None:
-                    m.bias.data.zero_()
-            elif isinstance(m, nn.Linear):
-                m.weight.data.normal_(0, 0.01)
-                m.bias.data.zero_()
+            if isinstance(m, (nn.Conv2d, nn.Linear)):
+                nn.init.orthogonal(m.weight())
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant(m.weight, 1)
+                nn.init.constant(m.bias, 0)
 
 
